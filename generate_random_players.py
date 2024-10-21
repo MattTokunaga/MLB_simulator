589dfa98ba02bdfa7bf672af418fa9a9842705c9
MattTokunaga <matttokunaga1@gmail.com>
2024-10-21 13:15:34 -0700

    implemented create mode for generate_random_players

--- generate_random_players.py
+++ generate_random_players.py
@@ -3,10 +3,12 @@ import sys
 import numpy as np
 import sqlite3
 import scipy
+from create_teams import create_teams
 
 roster_size = 26
 
 # regular position numbers except 10 is relief pitchers, 1 is starters only
+# based on a roster size of 26
 typical_roster = {
     1: 5,
     2: 2,
@@ -34,6 +36,7 @@ roster = {
     10: 8
 }
 
+# updates roster numbers based on given roster size
 while sum(roster.values()) != roster_size:
     distances = {
         key: typical_roster[key] * roster_size / 26 - roster[key] for key in typical_roster
@@ -47,11 +50,15 @@ while sum(roster.values()) != roster_size:
         argmax = max(distances, key = distances.get)
         roster[argmax] += 1
 
+# ensures mode is given
 try:
     mode = sys.argv[1]
 except:
     raise ValueError('no mode specified, please choose one of: populate, create, single')
 
+# ensures mode is either populate, create, or single
+# populate fills teams from the most recent year until they have full rosters
+# create makes teams for the most recent year + 1 then populates them
 if mode not in [
     'populate',
     'create',
@@ -59,13 +66,8 @@ if mode not in [
 ] :
     raise ValueError('Invalid arg')
 
-with open('first_names.txt') as f:
-    first_names = f.readlines()
-
-with open('last_names.txt') as f:
-    last_names = f.readlines()
-
-def generate_player(team, year, position = None, player_id = None):
+# function to create one random player
+def generate_player(team, year, position = None, player_id = None):    
     output = {
         'player_id': None,
         'handedness': None,
@@ -217,6 +219,8 @@ def generate_player(team, year, position = None, player_id = None):
     cur = con.cursor()
 
     if player_id:
+        if cur.execute('SELECT player_id FROM Players WHERE player_id = ?', [player_id]).fetchone():
+            raise ValueError('Player ID already exists')
         output['player_id'] = player_id
     else:
         max_player_id = cur.execute('SELECT MAX(player_id) FROM Players').fetchone()[0]
@@ -243,6 +247,7 @@ def generate_player(team, year, position = None, player_id = None):
 
     return output
 
+# function to insert player into database
 def insert_player(player):
     to_insert = [
         player['player_id'],
@@ -268,11 +273,10 @@ def insert_player(player):
     con.commit()
     return True
 
-con = sqlite3.connect('mlb_simulator.db')
-cur = con.cursor()
 
-
-if mode == 'populate':
+def populate(roster, verbose = False):
+    con = sqlite3.connect('mlb_simulator.db')
+    cur = con.cursor()
     year = cur.execute('SELECT MAX(year) FROM Teams').fetchone()[0]
     team_ids = cur.execute(f'SELECT team_id FROM Teams WHERE year = {year}').fetchall()
     for team in team_ids:
@@ -295,4 +299,23 @@ if mode == 'populate':
         for pos in pos_to_generate:
             for i in range(pos_to_generate[pos]):
                 insert_player(generate_player(team, year, pos))
+        if verbose:
+            print(f'Finished populating {team}')
+    
+
+if mode == 'populate':
+    populate(roster)
         
+if mode == 'create':
+    con = sqlite3.connect('mlb_simulator.db')
+    cur = con.cursor()
+    year = cur.execute('SELECT MAX(year) FROM Teams').fetchone()
+    if year:
+        year = year[0] + 1
+    else:
+        year = 1
+    create_teams(year)
+    populate(roster, True)
+
+if mode == 'single':
+    print("not implemented yet")
